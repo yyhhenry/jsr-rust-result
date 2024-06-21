842f48c01170d5d39bcacc08bb87c7aae8f0b2aa
yyhhenry <yyhhenry@foxmail.com>
2024-06-21 19:31:08 +0800

    chore: Remove module doc

--- mod.ts
+++ mod.ts
@@ -1,24 +1,4 @@
 /**
- * @module
- *
- * Rust-like error handling in TypeScript.
- *
- * @example
- * ```ts
- * import { ok, err, anyhow, safely } from "@yyhhenry/rust-result";
- *
- * const safeJsonParse = (s: string) => safely(() => JSON.parse(s));
- *
- * const result = safeJsonParse('{"a": 1}');
- * // Or
- * // const result = safeJsonParse('{"a": 1');
- * console.log(result.isOk() ? result.unwrap() : result.unwrapErr().message);
- * // It's safe to call unwrap() method in TypeScript,
- * // since it's only available when the type is narrowed to Ok.
- * ```
- */
-
-/**
  * Result type inspired by Rust's Result type.
  * - Try isOk() and isErr() to check the state of the instance.
  * - Use match() to handle the value or error.
